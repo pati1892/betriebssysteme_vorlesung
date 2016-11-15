f622310156c7ab5f2226a5bd3643bb38c1ee582a
Patrick <phenkelmann@online.de>
2016-11-15 14:22:34 +0100

    struct

--- C/struct/struct.c
+++ C/struct/struct.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct dir{
+	char path[255];
+	
+}Path;
+
 typedef struct file{
 	char name[255];
-	char directory[255];
+	Path path;
 	char owner[255];
 	int date; 
 	int size;
+	int permission;
 }File;
+
+
 
 void foo(File);
 
 int main(){
 	File file;
 	strcpy(file.name, "struct");
+	file.permission = 0b1010;
+	strcpy(file.path.path, "C/abc");
 	foo(file);
 
 }
@@ -21,4 +31,6 @@ int main(){
 void foo(File file){
 
 	printf("%s\n", file.name);
+	printf("%d\n", file.permission);
+	printf("%s\n", file.path.path);
 }
